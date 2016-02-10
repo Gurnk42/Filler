1f70c6714d95c994e2d4eebfb181086c5e4e0604
ebouther <ebouther@student.42.fr>
2016-02-10 14:53:09 +0100

    this is way better, but ai not finished yet

--- src/filler.c
+++ src/filler.c
@@ -2,6 +2,74 @@
 
 #include <stdio.h>
 
+static int	ft_test_placement(int i, int n, t_env *e)
+{
+	int x_center;
+	int y_center;
+	int x_i;
+	int y_i;
+	int x_pos;
+	int y_pos;
+	int	c;
+	int	touch;
+	char	pos_content;
+
+	touch = 0;
+	x_center = n % e->p_x;
+	y_center = n / e->p_x;
+	x_i = i % e->b_x;
+	y_i = i / e->b_x;
+
+	c = 0;
+	ft_putstr_fd("I == ", 2);
+	ft_putnbr_fd(i, 2);
+	ft_putchar_fd('\n', 2);
+	ft_putstr_fd("N == ", 2);
+	ft_putnbr_fd(n, 2);
+	ft_putchar_fd('\n', 2);
+	ft_putstr_fd("N center X == ", 2);
+	ft_putnbr_fd(x_center, 2);
+	ft_putchar_fd('\n', 2);
+	ft_putstr_fd("N center Y == ", 2);
+	ft_putnbr_fd(y_center, 2);
+	ft_putchar_fd('\n', 2);
+	while ((e->piece)[c])
+	{
+		if ((e->piece)[c] == '*')
+		{
+			x_pos = x_i + ((c % e->p_x) - x_center);
+			y_pos = y_i + ((c / e->p_x) - y_center);
+			ft_putstr_fd("pos of each *:\nY:  ", 2);
+			ft_putnbr_fd(y_pos, 2);
+			ft_putchar_fd('\n', 2);
+			ft_putstr_fd("pos of each *:\nX:  ", 2);
+			ft_putnbr_fd(x_pos, 2);
+			ft_putchar_fd('\n', 2);
+			ft_putstr_fd("CONTENT POS in grid *:\n:  ", 2);
+			ft_putnbr_fd((y_pos * e->b_x) + x_pos, 2);
+			ft_putchar_fd('\n', 2);
+			if (i < 0)
+				return (0);
+			if ((pos_content = (e->map)[(y_pos * e->b_x) + x_pos]) != '.')
+			{
+				if (pos_content == e->letter)
+					touch++;
+				if (touch > 1)
+					return (0);
+			}
+		}
+		c++;
+	}
+
+	ft_putchar_fd('\n', 2);
+	ft_putnbr_fd(x_center, 2);
+	ft_putchar_fd('\n', 2);
+	ft_putnbr_fd(y_center, 2);
+	ft_putchar_fd('\n', 2);
+	ft_putchar_fd('\n', 2);
+	return (1);
+}
+
 static void	ft_ai(t_env *e)
 {
 	int x_b = 0;
@@ -12,8 +80,10 @@ static void	ft_ai(t_env *e)
 	int	n;
 	int	x;
 	int	y;
+	int ret;
 
 	i = 0;
+	ret = -1;
 	while ((e->map)[i])
 	{
 		if ((e->map)[i] == e->letter)
@@ -24,11 +94,23 @@ static void	ft_ai(t_env *e)
 				if ((e->piece)[n] == '*')
 				{
 					x_p = n % e->p_x;
-					y_p = n / e->p_y;
+					y_p = n / e->p_x;
 
 					x_b = i % e->b_x;
-					y_b = (i / e->b_y) - 3;
+					y_b = (i / e->b_x);
+					ft_putstr_fd("n offset:\nX:  ", 2);
+					ft_putnbr_fd(x_p, 2);
 					ft_putchar_fd('\n', 2);
+					ft_putstr_fd("n offset:\nY:  ", 2);
+					ft_putnbr_fd(y_p, 2);
+					ft_putchar_fd('\n', 2);
+					ft_putstr_fd("WIDTH:\n  ", 2);
+					ft_putnbr_fd(e->p_x, 2);
+					ft_putchar_fd('\n', 2);
+					ft_putstr_fd("HEIGHT:\n  ", 2);
+					ft_putnbr_fd(e->p_y, 2);
+					ft_putchar_fd('\n', 2);
+					/*ft_putchar_fd('\n', 2);
 					ft_putnbr_fd(x_b, 2);
 					ft_putchar_fd('\n', 2);
 					ft_putnbr_fd(y_b, 2);
@@ -37,12 +119,14 @@ static void	ft_ai(t_env *e)
 					ft_putnbr_fd(x_p, 2);
 					ft_putchar_fd('\n', 2);
 					ft_putnbr_fd(y_p, 2);
-					ft_putchar_fd('\n', 2);
-					break ;
+					ft_putchar_fd('\n', 2);*/
+					if ((ret = ft_test_placement(i, n, e)) == 1)
+						break ;
 				}
 				n++;
 			}
-			break ;
+			if (ret == 1)
+				break ;
 		}
 		i++;
 	}
